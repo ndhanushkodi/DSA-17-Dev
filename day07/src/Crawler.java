926e128e4061577c9a42aca25efb3446b80c4103
David Abrahams <davidabrahams@sbcglobal.net>
2016-11-16 17:14:04 -0500

    starting writing bfs

--- day07/src/Crawler.java
+++ day07/src/Crawler.java
@@ -1,18 +1,45 @@
 import org.jsoup.select.Elements;
+import utilities.WikiFetcher;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.IOException;
+import java.util.*;
 
 public class Crawler {
-    public Elements crawl(String url) {
-        Queue<String> urls = new LinkedList<>();
-        urls.add(url);
 
-        //crawl
+    WikiFetcher fetcher;
+    Queue<Object[]> q;
+
+    public Crawler() {
+        this.fetcher = new WikiFetcher();
+        this.q = new LinkedList<>();
+    }
+
+    public Elements crawl(String url, int depth) {
+
         return null;
     }
 
-    public void findHyperlinks(Elements page) {
+    public void crawlHelper() throws IOException {
+
+        Set<String> visited = new HashSet<>();
+        while (!q.isEmpty()) {
+            Object[] urlDepth = q.remove();
+            String url = (String) urlDepth[0];
+            int depth = (Integer) urlDepth[1];
+
+            if (visited.contains(url))
+                continue;
+
+            Elements page = fetcher.fetchWikipedia(url);
+            for (String u : getHyperlinks(page).subList(0, 10)) {
+
+            }
+
+        }
+
+    }
+
+    public List<String> getHyperlinks(Elements page) {
 
     }
 }
